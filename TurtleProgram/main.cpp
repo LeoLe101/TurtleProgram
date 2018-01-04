69b3927cd5af06a71a706cbb4585a36ccdd945c0
LeoLe101 <p.le6796@edmail.edcc.edu>
2018-01-03 21:00:57 -0800

    Initializing Turtle Program for CSS 343 course

---
+++ TurtleProgram/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TurtleProgram
+//
+//  Created by Leo Le on 1/3/18.
+//  Copyright © 2018 Leo Le. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
